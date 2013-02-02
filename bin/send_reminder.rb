4c154d6050ab6cd234b4d0d888722d4f50bb2e02
Brian R. Jackson <brian@jaxzin.com>
2013-02-02 15:26:04 -0500

    Added support for postcard image.

--- bin/send_reminder.rb
+++ bin/send_reminder.rb
@@ -8,7 +8,7 @@ list = gb.lists({:filters => {:list_name => "Remember This Name Daily Reminders"
 
 puts "list = #{list}"
 
-campaign = gb.campaignCreate( {:type => :regular, :options => {:list_id => list['data'][0]['id'], :subject => "Today, Let's Remember", :from_email => "daily@rememberthisname.org", :from_name => "Remember This Name", :to_name => "Remember This Name Followers", :template_id => 22745, :generate_text => true }, :content => {:html => "<h1>Allison N. Wyatt</h1>She was a kind-hearted girl.", :html_std_preheader_content => "Allison was kind.", 'html_repeat_1:0:postcard_heading00' => "<h1>Today, Let's Remember</h1>", 'html_repeat_1:0:std_content00' => "<h1>Allison N. Wyatt</h1>She was a kind-hearted girl." } } )
+campaign = gb.campaignCreate( {:type => :regular, :options => {:list_id => list['data'][0]['id'], :subject => "Today, Let's Remember", :from_email => "daily@rememberthisname.org", :from_name => "Remember This Name", :to_name => "Remember This Name Followers", :template_id => 22765, :generate_text => true }, :content => {:html => "<h1>Allison N. Wyatt</h1>She was a kind-hearted girl.", :html_std_preheader_content => "Allison was kind.", 'html_repeat_1:0:postcard_heading00' => "<h1>Today, Let's Remember</h1>", 'html_repeat_1:0:std_content00' => "<h1>Allison N. Wyatt</h1>She was a kind-hearted girl.", 'html_repeat_1:0:postcard_image00' => "<img src='http://gallery.mailchimp.com/f0f4a75be6add63b318dd4c06/images/imageaf03dd.jpg' alt border='0' width='460' height='248'>" } } )
 
 puts "campaign = #{campaign}"
 
